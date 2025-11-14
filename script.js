efd72a7022a27dd6dbd3633566f629827aacc3e4
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-11-14 16:32:35 +0100

    feat: aggiunto bottone, genera email solo quando viene cliccato

--- script.js
+++ script.js
@@ -2,27 +2,35 @@
 // https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
+
 const emailList = document.getElementById("mail-list")
 
-let emails = [];
-for (let i = 0; i < 10; i++) {
-    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then(function (resp) {
-        const randomMail = resp.data.response;
-        emails.push(randomMail);
+
+const bottone = document.querySelector("button")
+console.log(bottone);
+
+bottone.addEventListener("click", function (event) {
+    event.preventDefault();
+    let emails = [];
+    for (let i = 0; i < 10; i++) {
+        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then(function (resp) {
+            const randomMail = resp.data.response;
+            emails.push(randomMail);
 
 
-        if (emails.length === 10) {
-            let lista = "";
-            for (let y = 0; y < emails.length; y++) {
-                lista += `<li>${emails[y]}</li>`;
-            }
-            emailList.innerHTML = lista;
-        };
+            if (emails.length === 10) {
+                let lista = "";
+                for (let y = 0; y < emails.length; y++) {
+                    lista += `<li>${emails[y]}</li>`;
+                }
+                emailList.innerHTML = lista;
+            };
 
 
 
-    });
+        });
 
-};
+    };
 
+})
 
